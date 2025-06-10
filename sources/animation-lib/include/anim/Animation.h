5814ccdb8c3d9e656cf2e54687115cba9c243988
Mark McManus <mcmmar@gmail.com>
2025-06-09 22:09:55 -0300

    Setup doxygen and started on some docs

--- sources/animation-lib/include/anim/Animation.h
+++ sources/animation-lib/include/anim/Animation.h
@@ -13,20 +13,92 @@
 
 namespace anim
 {
+    /**
+     * @brief Represents an animation system that manages animation variables, storyboards, and timer events.
+     * 
+	 * The `Animation` class uses the <a href="https://learn.microsoft.com/en-us/windows/win32/api/_uianimation/">Windows UI Animation Manager</a> to 
+	 * create and manage animations. It allows for the creation of animation variables and storyboards, and provides methods to set timer event handlers 
+     * and retrieve current time.
+     * 
+     * Animations currently only supports Timer-Driven Animations (rather than Application-Driven Animations).
+     */
     class Animation
     {
     public:
+        /**
+         * @brief Constructs an `Animation` object and initializes the animation manager and timer.
+         * 
+         * This constructor sets up the necessary components for animation management, including the animation manager and timer.
+		 */
         Animation();
+
+        /**
+         * @brief Destroys the `Animation` object and releases resources.
+         * 
+         * This destructor cleans up the animation manager and timer, ensuring that all resources are properly released.
+		 */
         ~Animation();
 
+        /**
+         * @brief Creates a new `AnimationVariable` with the specified initial value and an optional tag.
+         * @param initialValue The initial value to assign to the `AnimationVariable`.
+		 * @param tag An optional tag to associate with the `AnimationVariable`. If not provided, a default tag will be used. 
+		 * For more information see the documentation for `AnimationVariable`.
+         * @return A std::weak_ptr to the newly created `AnimationVariable`.
+		 * @remarks This is the prefered way to create and add an `AnimationVariable` to an animation (rather than 
+         * creating the `AnimationVariable` directly).
+         */
         std::weak_ptr<AnimationVariable> CreateVariable(double initialValue, std::optional<uint32_t> tag = std::nullopt);
+
+        /**
+         * @brief Creates a new `Storyboard` with an optional tag.
+         * @param tag An optional tag to associate with the `Storyboard`. If not provided, a default tag will be used.
+		 * @return A std::weak_ptr to the newly created `Storyboard`.
+         * @remarks This is the prefered way to create and add a `Storyboard` to an animation (rather than 
+         * creating the `Storyboard` directly).
+         */
         std::weak_ptr<Storyboard> CreateStoryboard(std::optional<uint32_t> tag = std::nullopt);
+
+        /**
+         * @brief Sets the event handler for timer events.
+         * @param eventHandler A pointer to the IUIAnimationTimerEventHandler interface that will handle timer events.
+         * @return true if the event handler was set successfully; false otherwise.
+         */
         bool SetTimerEventHandler(IUIAnimationTimerEventHandler* eventHandler);
+
+        /**
+         * @brief Retrieves the current time from the Animation manager.
+         * The time value return here can be used in the `Storyboard::Schedule()` function.
+         * @return An optional double containing the current time if it can be determined; otherwise, an empty optional.
+         */
         std::optional<double> GetTime();
 
+		/**
+		 * @brief Retrieves a weak pointer to the `AnimationVariable` associated with the specified tag.
+		 * @param tag The unique identifier (tag) of the `AnimationVariable` to retrieve.
+		 * @return A std::weak_ptr to the `AnimationVariable` corresponding to the given tag. If no variable is found, the returned weak pointer will be empty.
+		 */
 		std::weak_ptr<AnimationVariable> GetVariable(std::uint32_t tag) const;
+
+		/**
+		 * @brief Retrieves a std::weak_ptr to an `AnimationVariable` corresponding to the given `IUIAnimationVariable` pointer.
+		 * @param variable A pointer to the `IUIAnimationVariable` for which to obtain the `AnimationVariable`.
+		 * @return A std::weak_ptr to the associated `AnimationVariable` object. If no variable is found, the returned weak pointer will be empty.
+		 */
 		std::weak_ptr<AnimationVariable> GetVariable(IUIAnimationVariable* variable) const;
+
+        /**
+         * @brief Retrieves a weak pointer to the `Storyboard` associated with the specified tag.
+         * @param tag The tag identifying the desired `Storyboard`.
+         * @return A std::weak_ptr to the `Storyboard` corresponding to the given tag. If no such `Storyboard` exists, the returned weak pointer will be empty.
+         */
         std::weak_ptr<Storyboard> GetStoryboard(std::uint32_t tag) const;
+
+        /**
+         * @brief Retrieves a weak pointer to a `Storyboard` object associated with the given `IUIAnimationStoryboard` pointer.
+         * @param variable A pointer to an `IUIAnimationStoryboard` for which to obtain the corresponding `Storyboard`.
+         * @return A std::weak_ptr to the associated `Storyboard` object. If no such `Storyboard` exists, the returned weak pointer will be empty.
+         */
         std::weak_ptr<Storyboard> GetStoryboard(IUIAnimationStoryboard* variable) const;
 
 
--- sources/animation-lib/include/anim/AnimationVariable.h
+++ sources/animation-lib/include/anim/AnimationVariable.h
@@ -9,41 +9,174 @@
 
 namespace anim
 {
+    /**
+     * @brief Represents an animation variable that can be managed by the `Animation` class.
+     * 
+     * The `AnimationVariable` class creates and managers an `IUIAnimationVariable` object. When contructing an
+     * AnimationVariable an optional tag can be given, if the tag is not given one will be generated. Generated tags
+	 * start with the maximum value of `std::uint32_t` and are decremented for each new `AnimationVariable` created.
+     * 
+	 * While you can create an `AnimationVariable` directly is it recommended to use the `Animation::CreateVariable()` 
+     * function to create and manage `AnimationVariables`.
+     */
     class AnimationVariable
     {
     public:
+        /**
+         * @brief Constructs an `AnimationVariable` with the given manager and initial value.
+         * 
+         * If a tag is not provided, a unique tag will be generated.
+         * 
+         * @param manager The UI Animation Manager to use for this variable.
+         * @param initialValue The initial value of the animation variable.
+         * @param tag Optional tag for the variable. If not provided, a unique tag will be generated.
+         * 
+		 * @remarks It is recommended to use the `Animation::CreateVariable()` function to create and manage `AnimationVariable` instances.
+		 */
         AnimationVariable(
             IUIAnimationManager* manager, 
             double initialValue, 
             std::optional<uint32_t> tag);
+
+		/**
+        * @brief Destroys the `AnimationVariable` object and releases resources.
+        */
         ~AnimationVariable();
 
+        /**
+         * @brief Sets the handler to be called when a variable changes.
+         * 
+		 * The `VariableChangeHandler` will receive updated variables as `double` values. 
+         * 
+         * @param handler A pointer to a `VariableChangeHandler` that will handle variable change notifications.
+         * @return An HRESULT value indicating success or failure of setting the handler.
+         */
         HRESULT SetChangeHandler(VariableChangeHandler* handler);
+
+        /**
+         * @brief Sets the handler to be called when a variable changes.
+         *
+         * The `VariableIntegerChangeHandler` will receive updated variables as `int` values.
+         *
+         * @param handler A pointer to a `VariableIntegerChangeHandler` that will handle variable change notifications.
+         * @return An HRESULT value indicating success or failure of setting the handler.
+         */
         HRESULT SetChangeHandler(VariableIntegerChangeHandler* handler);
 
+        /**
+         * @brief Retrieves the value of the tag.
+         * 
+		 * The returned tag can be used to access the variable in the `Animation` class.
+         * 
+         * @return The current value of the tag as an unsigned 32-bit integer.
+         */
         std::uint32_t GetTag() const { return m_Tag; }
+
+        /**
+         * @brief Retrieves the value of the variable as a double.
+         * @return An optional containing the double value if available; otherwise, an empty optional.
+		 * @remarks If an empty optional is return call `GetError()` to retrieve the error code.
+         */
         std::optional<double> GetDouble();
+
+        /**
+         * @brief Retrieves the final value of the variable as a double.
+		 * @return An optional containing the final double value if available; otherwise, an empty optional.
+         * @remarks If an empty optional is returned, call `GetError()` to retrieve the error code.
+		 */
         std::optional<double> GetFinalDouble();
+
+        /**
+		 * @brief Retrieves the previous value of the variable as a double.
+         * @return An optional containing the previous double value if available; otherwise, an empty optional.
+         * @remarks If an empty optional is returned, call `GetError()` to retrieve the error code.
+		 */
         std::optional<double> GetPreviousDouble();
+
+        /**
+         * @brief Retrieves the value of the variable as an integer.
+         * @return An optional containing the integer value if available; otherwise, an empty optional.
+         * @remarks If an empty optional is returned, call `GetError()` to retrieve the error code.
+		 */
         std::optional<int> GetInteger();
+
+        /**
+         * @brief Retrieves the final value of the variable as an integer.
+         * @return An optional containing the final integer value if available; otherwise, an empty optional.
+		 * @remarks If an empty optional is returned, call `GetError()` to retrieve the error code.
+         */
         std::optional<int> GetFinalInteger();
+
+        /**
+         * @brief Retrieves the previous value of the variable as an integer.
+         * @return An optional containing the previous integer value if available; otherwise, an empty optional.
+		 * @remarks If an empty optional is returned, call `GetError()` to retrieve the error code.
+         */
         std::optional<int> GetPreviousInteger();
+
+        /**
+         * @brief Retrieves a pointer to the `IUIAnimationStoryboard` currently animating this variable.
+         * 
+		 * The returned can ve used with the `Animation::GetStoryboard()` function to retrieve the `Storyboard` 
+         * instance managed by the `Animation` class.
+         * 
+         * @return A pointer to an `IUIAnimationStoryboard` currently animating this variable.
+         */
         IUIAnimationStoryboard* GetStoryboard();
+
+		/**
+		 * @brief Sets the lower bounds for this variable.
+		 * @param lowerBound The lower bounds.
+		 * @return `true` if the lower bound was set successfully; otherwise, `false`.
+         * @remarks If `false` is returned, call `GetError()` to retrieve the error code.
+		 */
 		bool SetLowerBounds(double lowerBound);
+
+        /**
+         * @brief Sets the upper bounds for this variable.
+         * @param upperBound The upper bounds.
+         * @return `true` if the upper bound was set successfully; otherwise, `false`.
+         * @remarks If `false` is returned, call `GetError()` to retrieve the error code.
+		 */
 		bool SetUpperBounds(double upperBound);
+
+		/**
+		 * @brief Sets the rounding mode for UI animation calculations.
+		 * @param roundingMode The rounding mode to be applied to UI animation values.
+		 * @return Returns true if the rounding mode was set successfully; otherwise, returns false.
+         * @remarks If `false` is returned, call `GetError()` to retrieve the error code.
+		 */
 		bool SetRoundingMode(UI_ANIMATION_ROUNDING_MODE roundingMode);
 
+        /**
+         * @brief Checks whether the `AnimationVariable` is valid.
+         * @return true if the `AnimationVariable` valid and ready for use; otherwise, false.
+         */
         bool IsOk() const { return m_AnimationVariable != nullptr; }
+
+        /**
+         * @brief Retrieves the last error code produced for a call to a function in `AnimationVariable`.
+         * @return The HRESULT error code indicating the reason for failure.
+         * @remarks Call this function if any of the other function call in `AnimationVariable` fail.
+		 */
 		HRESULT GetError() const { return m_Error; }
 
+        /**
+         * @brief Retrieves the underlying `IUIAnimationVariable` object.
+         * 
+         * This function is mostly used internally and should only be called when functinality of the 
+         * UI Animation Manager is required that is currently not implemented in animation-lib.
+         * 
+         * @return A pointer to the `IUIAnimationVariable` object managed by this `AnimationVariable` instance.
+		 * @remarks If you do call this and plan to store the pointer be sure to call `AddRef()` and `Release()` 
+         * on the returned pointer to manage its lifetime correctly.
+		 */
         IUIAnimationVariable* GetVariable() const { return m_AnimationVariable; }
 
     private:
         std::uint32_t m_Tag{ 0 };
         HRESULT m_Error;
-
         IUIAnimationVariable* m_AnimationVariable{ nullptr };
-
         static std::uint32_t m_NextId;
     };
 }
